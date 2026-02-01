33651e0565acfd7213902d6838b8d2860ff6d2e8
SKD <skd1545passion@gmail.com>
2026-02-02 00:55:42 +0530

    Remove redirect route and associated validation from link.routes.js

--- server/routes/link.routes.js
+++ server/routes/link.routes.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { createLink } from "../controllers/link.controller.js";
-import { redirectToOriginal } from "../controllers/redirect.controller.js";
 import { apiKeyAuth } from "../middlewares/auth.middleware.js";
 import { rateLimit } from "../middlewares/rateLimiter.middleware.js";
 import { validateRequest } from "../middlewares/validate.middleware.js";
-import { createLinkBodySchema, redirectParamsSchema } from "../validations/link.validation.js";
+import { createLinkBodySchema } from "../validations/link.validation.js";
 
 const router = express.Router();
 
@@ -19,15 +18,4 @@ router.post(
     }),
     createLink
 );
-router.get(
-    "/:code",
-    validateRequest({ params: redirectParamsSchema }),
-    rateLimit({
-        windowSec: 60,
-        max: 100,
-        keyFn: (req) => req.ip
-    }),
-    redirectToOriginal
-);
-
 export default router;
